2db14ebe5acb94e27d3dc212844f313c099e926d
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-03-18 10:16:56 +0300

    commit tree 36967ca7941be2db5506205c09cda8cfff043d55

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = "Welcome to Flatbook,";
+const newwelcomeMessage = `Welcome to Flatbook, ${'Grace Hopper'}!`;
+
+
